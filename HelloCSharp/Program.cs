4e89a4a6032531fef569cfac8f144a650963a456
Faldi <faldiansyahk@gmail.com>
2025-04-05 01:11:32 +0700

    hello world

---
+++ HelloCSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Halo dari C#!");
+
+        Console.Write("Siapa nama kamu? ");
+        string nama = Console.ReadLine();
+        Console.WriteLine($"Halo, {nama}!");
+    }
+}
